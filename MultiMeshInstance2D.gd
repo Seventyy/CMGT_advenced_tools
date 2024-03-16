d3c6448cfab2bfd4ea362f671c49afcefca24c4f
Seventyy <seventyyyyy@gmail.com>
2024-03-16 17:38:54 +0100

    Initial commit

---
+++ MultiMeshInstance2D.gd
@@ -0,0 +1,14 @@
+@tool
+extends MultiMeshInstance2D
+
+
+@export var populate_random_:bool:
+	set(val):
+		populate_random()
+
+func populate_random() -> void:
+	for i in range(multimesh.instance_count):
+		var position = Transform2D()
+		position = position.translated(Vector2(randf_range(-100,100), randf_range(-100,100)))
+		multimesh.set_instance_transform_2d(i, position)
+
