ae0056b24fb7cb29bc899aae71f5651a41f59535
Seventyy <seventyyyyy@gmail.com>
2024-03-27 02:37:47 +0100

    grass semi final, low amount

--- MultiMeshInstance2D.gd
+++ MultiMeshInstance2D.gd
@@ -13,7 +13,7 @@ func populate_random() -> void:
 		var transform2D = Transform2D()
 		transform2D = transform2D.translated(Vector2(
 			randf_range(-box_size/2,box_size/2),
-			randf_range(-box_size/2,box_size/2)
+			(float(i)/multimesh.instance_count-.5) * box_size
 		))
 		positions.append(transform2D.origin)
 		multimesh.set_instance_transform_2d(i, transform2D)
