2239ec64cbb329b3c0505437e46dcc240382dcbf
Seventyy <seventyyyyy@gmail.com>
2024-04-25 07:31:07 +0200

    state: to much data

--- profiler.gd
+++ profiler.gd
@@ -1,8 +1,8 @@
 extends Node
 
-@export var time_per_mesh:float = 1
-@export var runs:int = 3
-@export var start_time:float = 1
+@export var time_per_mesh:float = 15
+@export var runs:int = 8
+@export var start_time:float = 10
 @export var end_time:float = 1
 
 @export var meshes:Array[Mesh]
@@ -15,7 +15,9 @@ extends Node
 var frames_elapsed:int
 var time_elapsed:float
 
-class TestSetData:
+var data:Array[TestData]
+
+class TestData:
 	var run_id:int
 	var grass_amount:int
 	var vertex_count:int
@@ -30,7 +32,7 @@ func _process(delta):
 
 func perform_tests():
 	await get_tree().create_timer(start_time).timeout
-	file = FileAccess.open("res://resoults.txt", FileAccess.WRITE)
+	file = FileAccess.open("res://resoults.csv", FileAccess.WRITE)
 	
 	for i in runs:
 		for mesh in meshes:
@@ -41,31 +43,53 @@ func perform_tests():
 				multi_mesh.populate_random()
 				
 				
-				start_profiler()
+				reset_profiler()
 				await get_tree().create_timer(time_per_mesh).timeout
-				end_profiler(mesh)
-			
-		file.store_string("\n")
-		
+				
+				var test_data := TestData.new()
+				test_data.run_id = i+1 
+				test_data.grass_amount = amount
+				test_data.vertex_count = int(
+					mesh.resource_path 
+					.trim_prefix("res://grass_meshes/grass_blade_") 
+					.trim_suffix(".obj")
+				)
+				test_data.avarage_frame_time = 1000 * time_elapsed / frames_elapsed
+				data.append(test_data)
+	
 	await get_tree().create_timer(end_time).timeout
+	save_resoults()
 	get_tree().quit()
 
-func start_profiler():
+func reset_profiler():
 	time_elapsed = 0
 	frames_elapsed = 0
 
-func end_profiler(mesh:Mesh):
-	file.store_string(
-		mesh.resource_path
-		 .trim_prefix("res://grass_meshes/grass_blade_")
-		 .trim_suffix(".obj") + "\t" + 
-		#str(frames_elapsed).pad_zeros(6) + "\t" +
-		#str(time_elapsed).pad_zeros(2).pad_decimals(15) + "\n" +
-		str(1000 * time_elapsed / frames_elapsed) + "\n"
-	)
+func save_resoults():
+	var last_run_id:int = 0
+	var last_vertex_count:int = 3
+	var line := PackedStringArray()
 	
-
-
-
-
+	for d in data:
+		if last_vertex_count != d.vertex_count:
+			line = PackedStringArray([str(last_vertex_count)]) + line
+			last_vertex_count = d.vertex_count
+			file.store_csv_line(line)
+			line.clear()
+		
+		if last_run_id != d.run_id:
+			last_vertex_count = 3
+			last_run_id = d.run_id
+			
+			file.store_string("\n")
+			
+			var headline := PackedStringArray()
+			headline.append(str(d.run_id))
+			for a in amounts:
+				headline.append(str(a))
+			file.store_csv_line(headline)
+		
+		line.append(str(d.avarage_frame_time))
+	
+	file.store_csv_line(PackedStringArray([str(last_vertex_count)]) + line)
 
